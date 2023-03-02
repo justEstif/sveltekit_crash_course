26f7413b7e624ffda4dd030aca72c4538cb84948
Estifanos <estifanosbeyene21@gmail.com>
2023-03-02 15:17:51 -0500

    feat: add toggle theme element

---
+++ 03_sveltekit_routing/src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import '@picocss/pico';
+	import Theme from '../lib/theme.svelte';
+</script>
+
+<main class="container">
+	<header>
+		<Theme />
+		<hgroup>
+			<h1>SvelteKit Routes</h1>
+		</hgroup>
+		<nav>
+			<ul>
+				<li>
+					<a href="/">Home</a>
+				</li>
+				<li>
+					<a href="/posts">Posts</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+
+	<slot />
+</main>
