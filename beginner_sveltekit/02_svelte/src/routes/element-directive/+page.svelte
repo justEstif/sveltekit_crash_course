9db9ee5ebddc867b63b1e0798511fdbd6ebe0dc4
Estifanos <estifanosbeyene21@gmail.com>
2023-03-06 08:15:55 -0500

    feat: svelte element directives

---
+++ beginner_sveltekit/02_svelte/src/routes/element-directive/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	function handleClick(event: MouseEvent) {
+		console.log(event);
+	}
+
+	let checked = false;
+	let searchedValue = '';
+</script>
+
+<h1>Element Directives</h1>
+<button on:click={() => alert('Clicked')}>Click Me!</button>
+
+<div on:mousemove|self|once={(e) => handleClick(e)} style="background: green; padding: 20px;">
+	You can click the green
+	<div style="background: orangered; padding: 20px;">You can't click the red</div>
+</div>
+
+<div>
+	<input bind:value={searchedValue} type="text" />
+	<p>searchedValue:{searchedValue}</p>
+
+	<input bind:checked type="checkbox" />
+	<p>checked:{checked}</p>
+</div>
